6d31f58ac3fe0f2851be67637a5e589321624ee9
Shaundai Person <shaundaiperson@gmail.com>
2021-05-31 17:20:21 -0400

    remove unused dependencies and update header section styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 
 import { HeaderText } from "./styles"
+import { Colors } from '../util/Colors'
 import { Emoji, device } from "../util/util"
 
 const HeaderContainer = styled.div`
@@ -13,14 +14,35 @@ const HeaderContainer = styled.div`
 `
 
 const CourseDescription = styled.div`
-	font-size: 2.3vw;
+	font-size: 2.2vw;
   font-weight: 600;
-	width: 70%;
+	width: 90%;
   margin-top: 29px;
   @media ${device.tablet} {
 		font-size: 16px;
     margin-top: 16px;
 	}
+  @media ${device.mobile} {
+		font-size: 12px;
+    margin-top: 16px;
+	}
+`
+
+const SmallHeaderText = styled.span`
+font-size: 2.3vw;
+font-family: "Montserrat", sans-serif;
+font-weight: bold;
+color: ${Colors.white};
+@media ${device.tablet} {
+  font-size: 16px;
+	}
+  @media ${device.mobile} {
+		font-size: 12px;
+	}
+`
+
+const SmallHeaderTextContainer = styled.div`
+margin-top: 20px;
 `
 
 export const Header = () => {
@@ -32,10 +54,13 @@ export const Header = () => {
 				<Emoji symbol="✨" label="sparkles" />.
 			</HeaderText>
 			<CourseDescription>
-				When you’re used to the carefree life of declaring variables without
-				having to worry about what they’ll eventually become or
-				the functions they’ll interact with, adding types to your code
-				just seems like <div>a bunch of extra work for little reward.</div>
+				<div>When you’re used to the carefree life of declaring variables</div>
+        <div>without worrying about what they’ll eventually become</div>
+				<div>or the functions they’ll interact with in their journey,</div>
+        <div>adding types to your code just seems like... </div>
+        <SmallHeaderTextContainer>
+          <SmallHeaderText>a bunch of extra work for little reward.</SmallHeaderText>
+        </SmallHeaderTextContainer>
 			</CourseDescription>
 		</HeaderContainer>
 	)
