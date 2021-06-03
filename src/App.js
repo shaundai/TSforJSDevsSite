e4b2e7db91020394605b9fec4b02332b94fe7158
Shaundai Person <shaundaiperson@gmail.com>
2021-06-02 21:25:23 -0400

    move styles out of components

--- src/App.js
+++ src/App.js
@@ -1,79 +1,10 @@
 import React from "react"
-import styled from "styled-components"
 
 import Shaundai from "./images/shaundaipic.png"
-import { Colors } from "./util/Colors"
 import { Footer } from "./components/Footer"
 import { SubscribeForm } from "./components/SubscribeForm"
 import { Header } from "./components/Header"
-import { HeaderText } from "./components/styles"
-import { device } from './util/util'
-
-const Container = styled.div`
-	background-color: ${Colors.mainBlack};
-	display: flex;
-	justify-content: center;
-	width: 100%;
-	color: ${Colors.mainWhite};
-	font-family: "Montserrat", sans-serif;
-`
-const Content = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	width: 90%;
-`
-
-const Title = styled.div`
-	display: flex;
-  flex-direction: column;
-	width: 100%;
-  height: 75vh;
-`
-
-const Line = styled.div`
-	display: flex;
-	text-align: center;
-	align-items: center;
-	justify-content: center;
-`
-
-const For = styled.em`
-	font-family: "Seaweed Script";
-	font-size: 4.5vw;
-	color: ${Colors.coolBlue};
-  margin: 0px 15px;
-  @media ${device.tablet} {
-		font-size: 2.3em;
-    margin: 0px 8px;
-	}
-`
-
-const InstructorPic = styled.img`
-	height: 4em;
-	border-radius: 50%;
-`
-
-const InstructorSection = styled.div`
-	height: 24vh;
-	display: flex;
-  justify-content: center;
-	align-items: center;
-  margin: 0 0 16vh;
-`
-
-const TaughtBy = styled.div`
-margin: 0px 10px;
-font-weight: 500;
-font-size: 22px;
-`
-
-const SignUp = styled.div`
-color: ${Colors.mainPurpleBright};
-width: 100%;
-text-align: center;
-`
+import { HeaderText, Container, Content, Title, Line, For, SignUp, InstructorPic, InstructorSection, TaughtBy } from "./components/styles"
 
 const App = () => {
 	return (
--- src/components/styles.js
+++ src/components/styles.js
@@ -22,4 +22,80 @@ background-clip: text;
 @media ${device.tablet} {
 		font-size: 2.3em;
 	}
+@media ${device.mobile} {
+		font-size: 1.3em;
+	}
+`
+
+export const Container = styled.div`
+	background-color: ${Colors.mainBlack};
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	color: ${Colors.mainWhite};
+	font-family: "Montserrat", sans-serif;
+`
+export const Content = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 90%;
+`
+
+export const Title = styled.div`
+	display: flex;
+  flex-direction: column;
+	width: 100vw;
+  height: 75vh;
+`
+
+export const Line = styled.div`
+	display: flex;
+	text-align: center;
+	align-items: center;
+	justify-content: center;
+	@media ${device.tablet} {
+		flex-direction: column;
+	}
+`
+
+export const For = styled.em`
+	font-family: "Seaweed Script";
+	font-size: 4.5vw;
+	color: ${Colors.coolBlue};
+  margin: 0px 15px;
+  @media ${device.tablet} {
+		font-size: 2.3em;
+    margin: 0px 8px;
+	}
+		@media ${device.mobile} {
+		font-size: 1.5em;
+    margin: 0px 8px;
+	}
+`
+
+export const InstructorPic = styled.img`
+	height: 4em;
+	border-radius: 50%;
+`
+
+export const InstructorSection = styled.div`
+	height: 24vh;
+	display: flex;
+  justify-content: center;
+	align-items: center;
+  margin: 0 0 16vh;
+`
+
+export const TaughtBy = styled.div`
+margin: 0px 10px;
+font-weight: 500;
+font-size: 22px;
+`
+
+export const SignUp = styled.div`
+color: ${Colors.mainPurpleBright};
+width: 100%;
+text-align: center;
 `
